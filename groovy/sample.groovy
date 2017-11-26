d4170cf748370399f198305cbbca730466749d50
rakesh635 <rakesh635@gmail.com>
2017-11-26 12:37:09 +0530

    modified sample.groovy

--- groovy/sample.groovy
+++ groovy/sample.groovy
@@ -2,5 +2,3 @@ import hudson.model.*
 import jenkins.model.Jenkins;
 import hudson.tasks.Shell;
 job = Jenkins.instance.createProject(FreeStyleProject, 'testjob1')
-job.buildersList.add(new Shell('echo hello world'))
-
