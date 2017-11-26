0fd9ff728686be294d5e1fe50636b384a4112b59
rakesh635 <rakesh635@gmail.com>
2017-11-26 12:44:39 +0530

    modified sample.groovy

--- groovy/sample.groovy
+++ groovy/sample.groovy
@@ -1,16 +1,3 @@
 import hudson.model.*
 import jenkins.model.Jenkins;
-import hudson.tasks.Shell;
-job = Jenkins.instance.createProject(FreeStyleProject, 'testjob1')
-job.buildersList.add(new Shell('echo hello world'))
-
-job.save()
-
-build = job.scheduleBuild2(5, new hudson.model.Cause.UserIdCause())
-
-build.get() # Block until the build finishes
-
-generatedJobs = build.getAction(javaposse.jobdsl.plugin.actions.GeneratedJobsBuildAction).getItems()
-
-# FIXME skip .scheduleBuild2() on Folder jobs
-generatedJobs.each { j -> j.scheduleBuild2(5, new hudson.model.Cause.UserIdCause()) }
+job = Jenkins.instance.createProject(FreeStyleProject, 'job-name')
